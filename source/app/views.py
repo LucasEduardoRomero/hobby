c1cc5c0c22d86d82826f0b3d2da9ce361414faf5
Felipe Jung <felipemattoseu@gmail.com>
2020-01-23 12:06:36 -0300

    Getting started part 2

--- source/app/views.py
+++ source/app/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h2 style='text-shadow:2px 2px 2px yellow'>Hello, world. You're at the app index.</h2>")
+    return HttpResponse("Hello, world. You're at the app index!! \o/")
+
+def detail(request, question_id):
+  return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+  response = ("You're looking at the results of question %s")
+  return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question  %s." % question_id)
