97f071ac3c87a593a2de9348720120902fb6b033
LucasEdRomero <lucasromero.cba@hotmail.com>
2020-01-24 11:08:06 -0300

    Adicionando Templates ao sistea

--- source/app/views.py
+++ source/app/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Question 
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the app index!! \o/")
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  template = loader.get_template('app/index.html')
+  context = {
+    'latest_question_list': latest_question_list,
+  }
+  return HttpResponse(template.render(context, request))
+
 
 def detail(request, question_id):
-  return HttpResponse("You're looking at question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'app/detail.html', {'question':question})
 
 def results(request, question_id):
   response = ("You're looking at the results of question %s")
